eb028d2deb4693922a3574348cf58356d55a340e
biri51 <joaogabrielvalenca@gmail.com>
2022-05-03 18:46:04 -0300

    transformacao inicial da proposta netflix/spotify

--- js/owl/setup.js
+++ js/owl/setup.js
@@ -1,7 +1,16 @@
 $('.owl-carousel').owlCarousel({
     loop:true,
+    fadeOut: true,
+    lazyLoad: true,
     margin:10,
     nav:false,
+    animateOut: 'fadeOut',
+    items:4,
+    margin:10,
+    stagePadding:30,
+    smartSpeed:450,
+    lazyLoad:true,
+    loop:true,
     responsive:{
         0:{
             items:1
